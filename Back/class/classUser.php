9192bb073ab55f7dc60f923acf41b359b1417cb1
MDLDeveloper <mariodanielledesma91@gmail.com>
2024-09-25 16:12:31 -0300

    Se comienza con la clase usuario

--- Back/class/classUser.php
+++ Back/class/classUser.php
@@ -1,6 +1,16 @@
 <?php 
     //Clase para representar un usuario
     class User {
-        
+        private $id_user;
+
+        public function __construct($id_user) {
+            $this->id_user = $id_user;
+        }
+
+        public function getIdUser() {
+            return $this->id_user;
+        }
+
+         
     }    
 ?>
