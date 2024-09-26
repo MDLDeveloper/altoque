9f21d338851adacfac47960d1141f0b221e516d0
MDLDeveloper <mariodanielledesma91@gmail.com>
2024-09-26 20:44:42 -0300

    Se comienza con User

--- back/class/classUser.php
+++ back/class/classUser.php
@@ -1,16 +1,41 @@
 <?php 
-    //Clase para representar un usuario
+    /*
+        Clase abstracta para modelar los usuarios y realizar las operaciones
+        necesarias para el sistema.
+    */
     class User {
         private $id_user;
+        private $dbconexion;
 
-        public function __construct($id_user) {
+        public function __construct(int $id_user, ConnectDB $dbconexion) {
             $this->id_user = $id_user;
+            $this->dbconexion = $dbconexion;
         }
+
+        public function ofrecerService(){}
+
+        public function contratarService(){}
+
+        public function agregarFavorito(){}
+
+        public function eliminarFavorito(){}
+
+        public function obtenerFavoritos(){}
+
+        public function agregarProyectos(){}
+
+        public function eliminarProyectos(){}
+
+        public function obtenerProyectos(){}
+
+        public function cambiarContrasena(){}
+
 
         public function getIdUser() {
             return $this->id_user;
         }
 
+        
          
     }    
 ?>
