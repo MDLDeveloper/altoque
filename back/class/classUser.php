5865c1787c5d82c75751e0bbd9a84e295a2eb66a
MDLDeveloper <mariodanielledesma91@gmail.com>
2024-09-27 06:57:51 -0300

    Time Breack para organizar las ideas.

--- back/class/classUser.php
+++ back/class/classUser.php
@@ -11,26 +11,47 @@
             $this->id_user = $id_user;
             $this->dbconexion = $dbconexion;
         }
-
+//Servicios
         public function ofrecerService(){}
 
         public function contratarService(){}
 
+        public function calificarServidor(){}
+
+        public function solicitarPresupuesto(){}
+//Favoritos
         public function agregarFavorito(){}
 
         public function eliminarFavorito(){}
 
         public function obtenerFavoritos(){}
-
+//Proyectos
         public function agregarProyectos(){}
 
         public function eliminarProyectos(){}
 
         public function obtenerProyectos(){}
 
+//Perfil y datos propios
         public function cambiarContrasena(){}
 
+        public function cambiarDatos(){}
 
+        public function eliminarCuenta(){}
+
+        public function obtenerDatos(){}
+
+//Notificaciones y mensajes
+        public function enviarMensaje(){}
+
+        public function obtenerMensajes(){}
+
+        public function eliminarMensaje(){}
+
+        public function obtenerNotificaciones(){}
+
+        
+        //Metodo getter ID del usuario
         public function getIdUser() {
             return $this->id_user;
         }
